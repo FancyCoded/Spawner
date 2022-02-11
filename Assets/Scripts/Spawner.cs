ad92fa28a4996b842a3e1c491dc8827cf091f6fb
SabyrbekovJanbolot <duisheevjanbolot@gmail.com>
2022-02-11 14:25:24 +0600

    stylecode improvements in spawner file

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,6 +8,17 @@ public class Spawner : MonoBehaviour
 
     private Transform[] _spawnPoints;
 
+    public IEnumerator Spawn()
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2);
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            yield return waitForTwoSeconds;
+            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
+        }
+    }
+
     private void Start()
     {
         _spawnPoints = new Transform[_points.childCount];
@@ -18,16 +29,5 @@ public class Spawner : MonoBehaviour
         }
 
         StartCoroutine(Spawn());
-    }
-
-    public IEnumerator Spawn()
-    {
-        var waitForTwoSeconds = new WaitForSeconds(2);
-
-        for(int i = 0; i < _spawnPoints.Length; i++)
-        {
-            yield return waitForTwoSeconds;
-            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
-        }
     }
 }
