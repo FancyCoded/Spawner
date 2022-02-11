023227cf893eb23e14f6c773330bc20f4244c3b9
SabyrbekovJanbolot <duisheevjanbolot@gmail.com>
2022-02-11 17:47:39 +0600

    code improvements

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,20 +4,9 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Transform _points;
-    [SerializeField] private GameObject _template;
+    [SerializeField] private Enemy _template;
 
     private Transform[] _spawnPoints;
-
-    public IEnumerator Spawn()
-    {
-        var waitForTwoSeconds = new WaitForSeconds(2);
-
-        for (int i = 0; i < _spawnPoints.Length; i++)
-        {
-            yield return waitForTwoSeconds;
-            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
-        }
-    }
 
     private void Start()
     {
@@ -29,5 +18,16 @@ public class Spawner : MonoBehaviour
         }
 
         StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2);
+
+        for (int i = 0; i < _spawnPoints.Length; i++)
+        {
+            yield return waitForTwoSeconds;
+            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
+        }
     }
 }
