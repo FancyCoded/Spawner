09e7edf566d357d8d54cd67434e95ecfda167343
SabyrbekovJanbolot <duisheevjanbolot@gmail.com>
2022-02-11 14:20:54 +0600

    init

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _points;
+    [SerializeField] private GameObject _template;
+
+    private Transform[] _spawnPoints;
+
+    private void Start()
+    {
+        _spawnPoints = new Transform[_points.childCount];
+        
+        for(int i = 0; i < _spawnPoints.Length; i++)
+        {
+            _spawnPoints[i] = _points.GetChild(i);
+        }
+
+        StartCoroutine(Spawn());
+    }
+
+    public IEnumerator Spawn()
+    {
+        var waitForTwoSeconds = new WaitForSeconds(2);
+
+        for(int i = 0; i < _spawnPoints.Length; i++)
+        {
+            yield return waitForTwoSeconds;
+            Instantiate(_template, _spawnPoints[i].position, Quaternion.identity);
+        }
+    }
+}
